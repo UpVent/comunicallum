d1bc85a422cb5a3098a72cbaf401872bd3f40103
VentGrey <omarpurataf@gmail.com>
2021-07-27 16:28:20 -0500

    Add universal allowed hosts

--- comunicallum/comunicallum/settings.py
+++ comunicallum/comunicallum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g(%gf3r7^h&a&kh0h94d&(rpyc#-8&_##r+wqugfmfzwuwzu*i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
