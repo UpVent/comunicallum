3bd13cd2451ef8ad5206f5c9d0a68f43ee3c41a3
VentGrey <omarpurataf@gmail.com>
2021-08-01 18:16:31 -0500

    Register model in admin.py

--- comunicallum/core/admin.py
+++ comunicallum/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contact
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'phone', 'email')
+    list_filter = ('full_name', 'email')
+    search_fields = ['full_name', 'phone', 'email']
+
+admin.site.register(Contact, ContactAdmin)
