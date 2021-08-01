a3339ded087d2233c48734261e8a5a031ba3d025
VentGrey <omarpurataf@gmail.com>
2021-08-01 18:31:49 -0500

    Begin working on contact form

--- comunicallum/core/views.py
+++ comunicallum/core/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+# For stderr logging
+import traceback
+import sys
 
-# Create your views here.
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
+from django.http import HttpResponse
+
+from django.conf import settings
+
+from .forms import ContactForm
+from .models import Contact
 
 def index(request):
     return render(request, "core/index.html", {})
