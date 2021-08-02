40366a717967d462d40efe6bfd2e68d15bc03b24
VentGrey <omarpurataf@gmail.com>
2021-08-02 17:58:13 -0500

    Added portfolio apps to urls

--- comunicallum/comunicallum/urls.py
+++ comunicallum/comunicallum/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('comunicadmin/', admin.site.urls),
     path('captcha/', include('captcha.urls')),
     # Main Site URLs
     path('', include('core.urls'), name='index'),
+    path('portfolio/', include('portfolio.urls', namespace="portfolio"), name='portfolio')
 ]
+
+# Special URL's for development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
