a92aa4d94978b35f3ce25a638e3df0b4ec0fc9c6
VentGrey <omarpurataf@gmail.com>
2021-08-02 18:05:47 -0500

    Register projects in admin.py

--- comunicallum/portfolio/admin.py
+++ comunicallum/portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_on', 'updated_on')
+    list_filter = ("name", "created_on", "updated_on",)
+    search_fields = ['name', 'description', 'created_on', 'updated_on']
+
+admin.site.register(Project, ProjectAdmin)
