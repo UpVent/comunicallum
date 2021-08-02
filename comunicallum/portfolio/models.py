e68c31d76ea1c4617b2c88f78bde99a9c1d093bc
VentGrey <omarpurataf@gmail.com>
2021-08-02 18:06:23 -0500

    Create a portfolio project model

--- comunicallum/portfolio/models.py
+++ comunicallum/portfolio/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+
+    image = models.ImageField(
+        verbose_name="Foto del proyecto",
+        upload_to="projects"
+    )
+
+    name = models.CharField(
+        verbose_name="Nombre del proyecto",
+        max_length=50,
+        unique=True,
+        help_text="Ingrese el nombre del proyecto",
+        default="",
+        blank=False
+    )
+
+    description = models.TextField(
+        verbose_name="Descripción corta del proyecto",
+        help_text="Ingrese una descripción corta del proyecto.",
+        default="",
+        blank=False
+    )
+
+    created_on = models.DateTimeField(
+        verbose_name="Creado el: ",
+        auto_now_add=True
+    )
+
+    updated_on = models.DateTimeField(
+        verbose_name="Actualizado el: ",
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['created_on']
+        verbose_name = "Proyecto de portafolio"
+        verbose_name_plural = "Proyectos del portafolio"
+
+    def __str__(self):
+        return self.name
