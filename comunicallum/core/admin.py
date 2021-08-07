d200525425f8ceb39d5f5b1a28bf5bb19df2de4d
VentGrey <omarpurataf@gmail.com>
2021-08-06 23:30:14 -0500

    Registered images in admin panel

--- comunicallum/core/admin.py
+++ comunicallum/core/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Contact
+from .models import Image
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'phone', 'email')
     list_filter = ('full_name', 'email')
     search_fields = ['full_name', 'phone', 'email']
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ['name', ]
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Image, ImageAdmin)
