ac76286e4a1d5baa3f24f0becf106e970ffc80b7
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-04 03:25:53 -0600

    Create a jumbotron component

---
+++ src/IndexJumbotron.svelte
@@ -0,0 +1,33 @@
+<script>
+    const logo = 'images/logo-web.png';
+</script>
+
+<section class="pt-4 mt-5 mb-5 pt-md-11">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-5 col-lg-6 order-md-2">
+            <img src="{logo}" height="350" width="350" class="img-fluid logo-img rounded-circle mb-6 mb-md-0" alt="logo">
+          </div>
+          <div class="col-12 col-md-7 col-lg-6 order-md-1">
+            <!-- Heading -->
+            <h1 class="display-4 text-center text-md-start">
+                Bienvenid@ a <span class="text-success">Comu</span><span class="text-primary">nica</span><span class="text-warning">llu</span><span class="text-danger">m</span>.
+            </h1>
+
+            <!-- Text -->
+            <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8">
+              Marketing Digital y Comunicación Estratégica para empresas.
+            </p>
+            <!-- Buttons -->
+            <div class="text-center text-md-start">
+              <a href="overview.html" class="btn btn-primary btn-lg bg-gradient shadow me-1">
+                Ver Portafolio <i class="bi bi-arrow-right d-none d-md-inline ms-3"></i>
+              </a>
+              <a href="docs/index.html" class="btn btn-secondary bg-gradient btn-lg">
+                Conócenos
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
