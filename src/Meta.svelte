daa6d2e84b318f3e7c97d4e5c0bd1f15e6c8fab1
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-14 22:47:02 -0600

    Add seo

--- src/Meta.svelte
+++ src/Meta.svelte
@@ -3,10 +3,39 @@
 </script>
 
 <SvelteSeo
-  title="Comunicallum - Agencia de Marketing Digital para tu negocio."
-  description="Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos."
-  keywords="Marketing Digital, Monterrey, Calidad, Negocio con poca inversión, Marketing, Agencia, Agencia de Marketing, UpVent, UpVent Technologies, VentGrey"
-  openGraph.tyle="website"
-  openGraph.description="Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos."
-    openGraph.url=""
+    title="Comunicallum - Agencia de Marketing Digital para tu negocio."
+    description="Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos."
+    keywords="Marketing Digital, Monterrey, Calidad, Negocio con poca inversión, Marketing, Agencia, Agencia de Marketing, UpVent, UpVent Technologies, VentGrey"
+
+    openGraph={{
+    title: "Comunicallum - Agencia de Marketing Digital para tu negocio.",
+    description: "Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos.",
+    type: "website",
+    url: "https://cominucallum.com/",
+    images: [
+      {
+        url: "https://comunicallum.com/images/opengraph.jpeg",
+        width: 850,
+        height: 650,
+        alt: "Logo Comunicallum Grande",
+      },
+    ],
+    }}
+
+    jsonLd={{
+        '@type': 'WebPage',
+        mainEntityOfPage: {
+        '@type': 'WebPage',
+        '@id': 'https://comunicallum.com/'
+        },
+        image: [
+        'https://comunicallum.com/images/opengraph.jpeg',
+        ],
+        datePublished: '2021-01-03T17:31:37Z',
+        author: {
+        '@type': 'Person',
+        name: 'El Equipo de Comunicallum'
+        },
+        }
+    }}
 />
