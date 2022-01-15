ac02f6601b6190a5a023f4f92e23f18509888a99
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-15 01:30:57 -0600

    Change section

--- src/Plans.svelte
+++ src/Plans.svelte
@@ -3,39 +3,49 @@
         <div class="row">
           <div class="col-12 col-md-4">
             <div class="text-primary mb-3">
-                <svg width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M7 3h10a4 4 0 110 8H7a4 4 0 110-8zm0 6a2 2 0 100-4 2 2 0 000 4z" fill="#2EA690"></path><path d="M7 13h10a4 4 0 110 8H7a4 4 0 110-8zm10 6a2 2 0 100-4 2 2 0 000 4z" fill="#2EA690" opacity=".3"></path></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="text-warning bi bi-trophy" viewBox="0 0 16 16">
+  <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935zM3.504 1c.007.517.026 1.006.056 1.469.13 2.028.457 3.546.87 4.667C5.294 9.48 6.484 10 7 10a.5.5 0 0 1 .5.5v2.61a1 1 0 0 1-.757.97l-1.426.356a.5.5 0 0 0-.179.085L4.5 15h7l-.638-.479a.501.501 0 0 0-.18-.085l-1.425-.356a1 1 0 0 1-.757-.97V10.5A.5.5 0 0 1 9 10c.516 0 1.706-.52 2.57-2.864.413-1.12.74-2.64.87-4.667.03-.463.049-.952.056-1.469H3.504z"/>
+</svg>
             </div>
             <h3>
-              Planes variados
+              Misión
             </h3>
-            <p class="text-muted mb-6 mb-md-0">
-              Ofrecemos una variedad de planes para usted y para su empresa. Diseñados para llevarle al éxito en poco tiempo.
-            </p>
+            <p class="text-muted text-wrap text-break mb-6 mb-md-0">
+              Somos una organizacion de luz con la misión de desarrollar e impulsar a un crecimiento exponencial a marcas y organizaciones del ámbito deportivo a través de una comunicación estratégica digital productiva. Para ayudarlos a gestionar recursos por medio de nuestros servicios del marketing digital y así puedan crecer sus comunidades de luz.            </p>
           </div>
           <div class="col-12 col-md-4">
             <!-- Icon -->
             <div class="text-primary mb-3">
-              <svg width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M5.5 4h4A1.5 1.5 0 0111 5.5v1A1.5 1.5 0 019.5 8h-4A1.5 1.5 0 014 6.5v-1A1.5 1.5 0 015.5 4zm9 12h4a1.5 1.5 0 011.5 1.5v1a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-1a1.5 1.5 0 011.5-1.5z" fill="#335EEA"></path><path d="M5.5 10h4a1.5 1.5 0 011.5 1.5v7A1.5 1.5 0 019.5 20h-4A1.5 1.5 0 014 18.5v-7A1.5 1.5 0 015.5 10zm9-6h4A1.5 1.5 0 0120 5.5v7a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-7A1.5 1.5 0 0114.5 4z" fill="#335EEA" opacity=".3"></path></g></svg>            </div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="text-danger bi bi-bullseye" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M8 13A5 5 0 1 1 8 3a5 5 0 0 1 0 10zm0 1A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
+  <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8z"/>
+  <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+</svg>
+    </div>
 
             <!-- Heading -->
             <h3>
-              Diseño modeno
+              Visión
             </h3>
             <!-- Text -->
-            <p class="text-muted mb-6 mb-md-0">
-              Nuestros planes de Marketing están diseñados con las últimas tendencias. Su empresa se sentirá moderna, minimalista y exitosa.
+            <p class="text-muted text-wrap text-break mb-6 mb-md-0">
+              Para el 2025 estaremos posicionados a nivel nacional por nuestra labor de impulsar el deporte en la sociedad especialmente en los más jóvenes a través de la comunicación y su difusión.
             </p>
           </div>
           <div class="col-12 col-md-4">
 
             <!-- Icon -->
             <div class="text-primary mb-3">
-              <svg width="64" height="64" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M17.272 8.685a1 1 0 111.456-1.37l4 4.25a1 1 0 010 1.37l-4 4.25a1 1 0 01-1.456-1.37l3.355-3.565-3.355-3.565zm-10.544 0L3.373 12.25l3.355 3.565a1 1 0 01-1.456 1.37l-4-4.25a1 1 0 010-1.37l4-4.25a1 1 0 011.456 1.37z" fill="#D91818"></path><rect fill="#D91818" opacity=".3" transform="rotate(15 12 12)" x="11" y="4" width="2" height="16" rx="1"></rect></g></svg>            </div>
+              <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="text-dark bi bi-yin-yang" viewBox="0 0 16 16">
+  <path d="M9.167 4.5a1.167 1.167 0 1 1-2.334 0 1.167 1.167 0 0 1 2.334 0Z"/>
+  <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1 8a7 7 0 0 1 7-7 3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 0 0 7 7 7 0 0 1-7-7Zm7 4.667a1.167 1.167 0 1 1 0-2.334 1.167 1.167 0 0 1 0 2.334Z"/>
+</svg>            </div>
             <h3>
-              Desarrollo de páginas web
+              Filosofía
             </h3>
-            <p class="text-muted mb-0">
-              La página web de su negocio es un elemento crucial en el mundo actual. Nuestras páginas optimizadas lo tendrán siempre cubierto.
+            <p class="text-muted text-wrap text-break mb-0">
+              Todos somos uno solo. Creemos firmemente en que ayudar a tu entorno es ayudarse a sí mismo y viceversa. Por eso tratamos de colaborar con personas que compartan nuestra visión de comunicar luz, es decir conocimiento, valores, experiencias; inspirar a través de lo que comunicamos para generar una comunidad que transforme con la comunicación.
             </p>
           </div>
         </div>
