689815686179ac06155dc0130924e3d94c1434d9
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-14 21:34:07 -0600

    Add images and services boilerplate for dynamic weas

--- src/Services.svelte
+++ src/Services.svelte
@@ -1,3 +1,11 @@
+<script>
+    let services = [{
+        image: '',
+        name: 'cosa',
+        description: '',
+    }]
+</script>
+
 <section class="container px-4 py-5">
     <h2 class="display-5 fw-bold pb-2 border-bottom">Nuestros servicios</h2>
     <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
@@ -7,6 +15,13 @@
         </div>
         <h2>PCM</h2>
         <p>Optimizamos tus campañas de publicidad en redes sociales, asegurando de que tu inversión en las mismas genere nuevos clientes.</p>
+        <br>
+        <p>Además de optimizar tus campañas de publicidad te ofrecemos:</p>
+        <ul class="text-muted">
+            <li>Cobertura de eventos deportivos <i class="bi bi-activity"></i></li>
+            <li>Creación y difusión de contenido digital <i class="bi bi-graph-up-arrow"></i></li>
+            <li>Marketing de Emails <i class="text-dark bi bi-envelope-check"></i></li>
+        </ul>
       </div>
       <div class="feature col">
         <div class="rounded-3 d-inline-flex bg-success bg-gradient">
@@ -22,5 +37,6 @@
         <h2>Estrategia Digital</h2>
         <p>Te ayudaremos a incrementar las ventas de tu negocio a través de una estrategia digital para que obtengas los mejores resultados.</p>
       </div>
-    </div>
+  </div>
+
 </section>
