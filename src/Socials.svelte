24fc7b30dcb36b8cdccf1c851407729b4a726331
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-15 02:08:21 -0600

    Add socials

--- src/Socials.svelte
+++ src/Socials.svelte
@@ -1,19 +1,28 @@
 <script charset="utf-8">
-    
+    let screenshot = 'images/screen.jpeg';
 </script>
 
 <style type="text/css" media="screen">
-    
+    .img-spot {
+        transform: rotate(20deg);
+    }
 </style>
 
 <section class="mt-2 mb-2">
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h1>¡Síguenos en nuestras redes sociales!</h1>
+            <div class="col-md-8">
+                <h1 class="display-3 border-bottom">¡Síguenos en nuestras redes sociales!</h1>
+                <p class="lead">Subimos contenido de forma regular, así como coberturas de eventos, tips y mucho más.</p>
+                <a class="btn btn-primary text-white border-0" style="background-color: #3b5998;" href="https://www.facebook.com/Comunicallum/" role="button"><i class="bi bi-facebook"></i></a>
+
+                <a class="btn btn-primary text-white border-0" style="background-color: #ac2bac;" href="https://www.instagram.com/comunicallum/" role="button"><i class="bi bi-instagram"></i></a>
+
             </div>
-            <div class="col">
-                <img src="" alt=""/>
+            <div class="col-md-4">
+                <a href="https://www.instagram.com/comunicallum/" target="_blank">
+                    <img class="img-fluid rounded shadow img-spot" src={screenshot} alt=""/>
+                </a>
             </div>
         </div>
     </div>
