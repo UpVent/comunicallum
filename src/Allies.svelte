b120c14853b3306ccd0e503d432a51b89f0a787c
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-17 20:48:17 -0600

    Add logos

--- src/Allies.svelte
+++ src/Allies.svelte
@@ -2,9 +2,16 @@
     let logo1 = 'images/logo1.png';
     let logo2 = 'images/logo2.png';
     let logo3 = 'images/logo3.jpg';
-    let logo4 = 'images/logo4.jpg';
+    let logo4 = 'images/logo4.png';
     let logo5 = 'images/logo5.png';
     let logo6 = 'images/logo6.png';
+    let logo7 = 'images/logo7.png';
+    let logo8 = 'images/logo8.png';
+    let logo9 = 'images/logo9.png';
+    let logo10 = 'images/logo10.png';
+    let logo11 = 'images/logo11.png';
+    let logo12 = 'images/logo12.png';
+    let logo13 = 'images/logo13.png';
 </script>
 
 <section class="py-6 py-md-8 border-bottom">
@@ -45,6 +52,49 @@
                   <img width="145" height="46" class="img-fluid" src={logo6} alt="logo 6"/>
               </div>
           </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo7} alt="logo 7"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo8} alt="logo 8"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo9} alt="logo 9"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo10} alt="logo 10"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo11} alt="logo 11"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo12} alt="logo 12"/>
+              </div>
+          </div>
+
+          <div class="col-6 col-sm-4 col-md-2 mb-4 mb-md-0">
+              <div class="img-fluid text-gray-600 mb-2 mb-md-0">
+                  <img width="145" height="46" class="img-fluid" src={logo13} alt="logo 13"/>
+              </div>
+          </div>
+
         </div>
       </div>
 </section>
