bd90d628153841f7e4d0cc558fd66eee3949fcd6
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-01-20 01:40:38 -0600

    Remove meta component due to name conflicts

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,14 @@
     import Footer from './Footer.svelte';
     import IndexJumbotron from './IndexJumbotron.svelte';
     import Medal from './Medal.svelte';
-    import Meta from './Meta.svelte';
     import Navbar from './Navbar.svelte';
     import Plans from './Plans.svelte';
     import Services from './Services.svelte';
     import Socials from './Socials.svelte';
     import TopBtn from './TopBtn.svelte';
+
+    // External import
+    import SvelteSeo from 'svelte-seo';
 
     /* GetButton.io widget */
     (function () {
@@ -34,7 +36,43 @@
 </svelte:head>
 
 <!-- SEO component -->
-<Meta/>
+<SvelteSeo
+    title="Comunicallum - Agencia de Marketing Digital para tu negocio."
+    description="Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos."
+    keywords="Marketing Digital, Monterrey, Calidad, Negocio con poca inversión, Marketing, Agencia, Agencia de Marketing, UpVent, UpVent Technologies, VentGrey"
+
+    openGraph={{
+    title: "Comunicallum - Agencia de Marketing Digital para tu negocio.",
+    description: "Comunicallum es una agencia de marketing digital dedicada al aumento de alcance, optimización y propio manejo de la información en los negocios Mexicanos.",
+    type: "website",
+    url: "https://cominucallum.com/",
+    images: [
+      {
+        url: "https://comunicallum.com/images/opengraph.jpeg",
+        width: 850,
+        height: 650,
+        alt: "Logo Comunicallum Grande",
+      },
+    ],
+    }},
+
+    jsonLd={{
+        '@type': 'WebPage',
+        mainEntityOfPage: {
+        '@type': 'WebPage',
+        '@id': 'https://comunicallum.com/'
+        },
+        image: [
+        'https://comunicallum.com/images/opengraph.jpeg',
+        ],
+        datePublished: '2021-01-03T17:31:37Z',
+        author: {
+        '@type': 'Person',
+        name: 'El Equipo de Comunicallum'
+        },
+        }
+    }}
+/>
 
 <!-- Navbar component -->
 <Navbar/>
