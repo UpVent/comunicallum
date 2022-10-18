51ccc363aff134e92a853d97b3320c1f5c139a35
VentGrey <omarpurataf@gmail.com>
2022-10-17 22:52:37 -0500

    More optimizations

--- src/App.svelte
+++ src/App.svelte
@@ -24,10 +24,10 @@
             order: "facebook,whatsapp", // Order of buttons
             pre_filled_message: "¡Hola me comunico desde tu página web!", // WhatsApp pre-filled message
         };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        let proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        let s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+            let x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     })();
 
     // SEO variables
